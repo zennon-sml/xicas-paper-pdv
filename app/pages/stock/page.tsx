fa825c2d1a5c6d7267d999074b6341b71ab88f7f
X1K00 <fcju14@gmail.com>
2024-12-20 14:35:46 -0300

    creation of a table of products in stock

--- app/pages/stock/page.tsx
+++ app/pages/stock/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 export default function Stock() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col h-screen'>
       <SideBar />
 
       <div className=' flex fixed left-12 bg-[#8BE8DC] w-[calc(100%-3rem)] h-11 pr-3 pb-1 pt-1 '>
@@ -20,16 +20,49 @@ export default function Stock() {
         <button className='pr-3 pl-3 pt-1 pb-1 text-[#0B625D] border-t-[2px] hover:border-t-[#4DC5BD] hover:bg-[#CBFCF6] hover:z-10 hover:font-bold'>LIXEIRA</button>
       </div>
 
-      <div className='flex w-screen h-screen pt-6 pl-5 bg-[#CBFCF6] gap-2'>
-        <input type="search" placeholder="Pesquisar por nome, codigo de barras..." className="flex bg-[#8BE8DC] placeholder-[#46b0a9] text-[#0B625D] text-sm h-7 w-80 pl-2 hover:bg-[#68dbcb]"/>
-        <select id='tags' name='tags' className="flex bg-[#8BE8DC] placeholder-[#46b0a9] text-[#0B625D] text-sm h-7 w-40 pl-2 hover:bg-[#68dbcb]">
-          <option value="todos">Todos</option>
-          <option value="todos">#creu</option>
-          <option value="todos">#vrau</option>
-        </select>
+      <div className='flex flex-col flex-grow bg-[#CBFCF6] overflow-hidden'>
+        <div className='flex pt-6 pl-5 gap-2'>
+          <input type="search" placeholder="Pesquisar por nome, codigo de barras..." className="flex bg-[#8BE8DC] placeholder-[#46b0a9] text-[#0B625D] text-sm h-7 w-80 pl-2 hover:bg-[#68dbcb]"/>
+          <select id='tags' name='tags' className="flex bg-[#8BE8DC] placeholder-[#46b0a9] text-[#0B625D] text-sm h-7 w-40 pl-2 hover:bg-[#68dbcb]">
+            <option value="todos">Todos</option>
+            <option value="todos">#creu</option>
+            <option value="todos">#vrau</option>
+          </select>
+        </div>
 
-        <div>
+        <p className=' m-3'>0 produtos</p>
 
+        <div className='flex flex-col flex-grow rounded-md overflow-hidden bg-[#E4FFFC] m-2'>
+          <table className='w-full'>
+            <thead className='bg-[#8BE8DC]'>
+                <tr className="text-sm text-[#397F7B]">
+                    <th className=" w-1/12">Item</th>
+                    <th className=" w-5/12">Produto</th>
+                    <th className=" w-1/12">Qtd</th>
+                    <th className=" w-2/12">Desconto</th>
+                    <th className=" w-2/12">P.Unit</th>
+                    <th className=" w-3/12">Total</th>
+                </tr>
+            </thead>
+            <tbody className="bg-[#B8FFF7] text-xs overflow-y-auto h-full">
+              <tr className="border border-[#198A83] bg-white">
+                <td className="pl-1 pr-1 text-[#135550] font-bold text-center">creu</td>
+                <td className="pl-1 pr-1 text-[#135550]"></td>
+                <td className="pl-1 pr-1 text-[#135550] text-center"></td>
+                <td className="pl-1 pr-1 text-[#135550] text-center"></td>
+                <td className="pl-1 pr-1 text-[#135550]"></td>
+                <td className="pl-1 pr-1 text-[#135550] font-bold"></td>
+              </tr>
+              <tr className="border border-[#198A83] bg-white">
+                <td className="pl-1 pr-1 text-[#135550] font-bold text-center">vrau</td>
+                <td className="pl-1 pr-1 text-[#135550]"></td>
+                <td className="pl-1 pr-1 text-[#135550] text-center"></td>
+                <td className="pl-1 pr-1 text-[#135550] text-center"></td>
+                <td className="pl-1 pr-1 text-[#135550]"></td>
+                <td className="pl-1 pr-1 text-[#135550] font-bold"></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
