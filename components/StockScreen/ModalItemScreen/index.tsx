8c7cce92c791def2dce46732c5b5e97d86ae62e1
X1K00 <fcju14@gmail.com>
2025-01-14 21:51:01 -0300

    update modal item stock

--- components/StockScreen/ModalItemScreen/index.tsx
+++ components/StockScreen/ModalItemScreen/index.tsx
@@ -2,7 +2,17 @@ interface ModalItemProps {
     showModal: boolean;
     closeModal: () => void;
 }
-
+const lista = [
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg",
+    "/img/iphone13.jpg"
+]
 export default function ModalItemScreen({showModal, closeModal}:ModalItemProps) {
     return(
         <div>
@@ -16,133 +26,80 @@ export default function ModalItemScreen({showModal, closeModal}:ModalItemProps) 
                         className="
                         bg-white rounded-lg p-6 gap-4 w-auto
                         flex justify-between"
-                    >
-                        <div className="flex flex-col p-2 gap-3 bg-[#CBFCF6] rounded-md">
-                            <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Venda Total:</label>
-                                    <input 
-                                    type="text" 
-                                    disabled/* readOnly */
-                                    className='w-60 h-9 p-2 bg-[#46b0a9] text-white  rounded-md text-lg'
-                                    
-                                    />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Tipo Desconto:</label>
-                                <select 
-                                id='tags' 
-                                name='tags' 
-                                className="w-60 h-9 p-1 bg-[#3BDCD2] text-[#198A83] rounded-md">
-                                    <option value="valor">Valor (R$)</option>
-                                    <option value="porcentagem">Porcentagem (%)</option>
-                                </select>
-                            </div>
+                    >   
 
-                            <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Total Desconto:</label>
-                                    <input 
-                                    type="number" 
-                                    className='w-60 h-9 p-2 bg-[#3BDCD2] text-[#198A83]  rounded-md'
-                                    min={0}
-                                    />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Valor Final:</label>
-                                    <input 
-                                    type="text" 
-                                    disabled/* readOnly */
-                                    className='w-60 h-9 p-2 bg-[#46b0a9] text-white  rounded-md text-lg'
-                                    value={""}  
-                                    />
+                        <div className="flex gap-3">
+                            <div className="flex border border-[#81f7ef] rounded-md overflow-hidden">
+                                <div className="flex bg-white h-36 overflow-hidden">
+                                    <img 
+                                    alt="img-vazia" 
+                                    src={"/img/sem-foto.jpg"}
+                                    className='bg-[#ffffff] p-1 object-contain object-center'/>
+                                </div>
+
+                                <div className="flex flex-col overflow-auto h-36 border border-l-[#81f7ef]">
+                                    {lista.map((image, i) =>
+                                        <img className="bg-[#ffffff] w-10 h-10 p-1 object-contain object-center" src={image} key={i} alt={`imagem ${i}`} />
+                                    )}
+                                </div>
                             </div>
                             
-                        </div>
-
-
-                        <div className="flex flex-col gap-4">
-                            <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Tipo de Pagamento:</label>
-                                <select 
-                                id='tags' 
-                                name='tags' 
-                                className="w-60 h-9 p-1 bg-[#3BDCD2] text-[#198A83] rounded-md">
-                                    <option value="money">Dinheiro</option>
-                                    <option value="pix">PIX</option>
-                                    <option value="debito">Debito</option>
-                                    <option value="credito">Credito</option>
-                                    <option value="outro">Outro</option>
-                                </select>
-                            </div>
-
-
-                            {/* Pagamento em dinheiro */}
-                            <div className="flex flex-col p-2 gap-3 bg-[#CBFCF6] rounded-md">
-                                <div className="flex flex-col">
-                                    <label className='text-sm text-[#198A83]'>Valor Recebido:</label>
+                            <div className="flex flex-col gap-1">
+                                <div className="flex gap-1">
+                                    <div className="flex flex-col">
+                                        <label className='text-xs text-[#198A83]'>Nome do Produto:</label>
                                         <input 
-                                        type="number" 
-                                        className='w-60 h-9 p-2 bg-[#3BDCD2] text-[#198A83]  rounded-md'
-                                        min={0}
+                                        type="text" 
+                                        className='w-96 h-8 p-2 bg-[#46b0a9] text-white  rounded-md text-sm'
                                         />
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='text-xs text-[#198A83]'>ID:</label>
+                                        <input 
+                                        type="text" 
+                                        disabled
+                                        className='w-28 h-8 p-2 bg-[#46b0a9] text-white  rounded-md text-sm'
+                                        />
+                                    </div>
                                 </div>
+
                                 <div className="flex flex-col">
-                                <label className='text-sm text-[#198A83]'>Troco:</label>
+                                    <label className='text-xs text-[#198A83]'>Cod. de Barras:</label>
                                     <input 
                                     type="text" 
-                                    disabled/* readOnly */
-                                    className='w-60 h-9 p-2 bg-[#46b0a9] text-white  rounded-md text-lg'
-                                    value={""}  
+                                    className='h-8 p-2 bg-[#46b0a9] text-white  rounded-md text-sm'
                                     />
                                 </div>
+                                
+                                <div className="flex gap-1 justify-between">
+                                    <div className="flex flex-col">
+                                        <label className='text-xs text-[#198A83]'>Quantidade:</label>
+                                            <input 
+                                            type="number" 
+                                            className='max-w-40 h-8 p-2 bg-[#3BDCD2] text-[#198A83] text-sm rounded-md'
+                                            min={0}
+                                            />
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='text-xs text-[#198A83]'>Custo Unit.:</label>
+                                            <input 
+                                            type="number" 
+                                            className='min-w-40 h-8 p-2 bg-[#3BDCD2] text-[#198A83] text-sm rounded-md'
+                                            min={0}
+                                            />
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='text-xs text-[#198A83]'>Custo Total:</label>
+                                            <input 
+                                            type="number" 
+                                            className='max-w-40 h-8 p-2 bg-[#3BDCD2] text-[#198A83] text-sm rounded-md'
+                                            min={0}
+                                            />
+                                    </div>
+                                </div>
                             </div>
-
-                            <div className="flex justify-between gap-2">
-                                <button
-                                onClick={closeModal} // Fecha o modal
-                                className="bg-red-500 text-white py-2 px-4 rounded-md"
-                                >
-                                    Cancelar
-                                </button>
-                                <button
-                                    onClick={() => {
-                                    closeModal();
-                                    alert("Venda finalizada com sucesso!"); // Exemplo de ação final
-                                    }}
-                                    className="bg-green-500 text-white py-2 px-4 rounded-md w-full"
-                                >
-                                    Confirmar
-                                </button>
-                            </div>
-
-                        </div>
-                        
-                        
-                        {/* <h2 className="text-lg font-bold text-[#198A83] mb-4">
-                        Confirmação
-                        </h2>
-                        <p className="text-gray-700 text-center mb-4">
-                        Deseja realmente finalizar a venda no valor de{" "}
-                        <span className="font-bold">R$ {totalValue.toFixed(2)}</span>?
-                        </p>
-                        <div className="flex gap-4">
-                        <button
-                            onClick={closeModal} // Fecha o modal
-                            className="bg-red-500 text-white py-2 px-4 rounded-md"
-                        >
-                            Cancelar
-                        </button>
-                        <button
-                            onClick={() => {
-                            closeModal();
-                            alert("Venda finalizada com sucesso!"); // Exemplo de ação final
-                            }}
-                            className="bg-green-500 text-white py-2 px-4 rounded-md"
-                        >
-                            Confirmar
-                        </button>
-                        </div> */}
-
-                    </div>
+                        </div>    
+                    </div>     
                 </div>
             )}
         </div>
