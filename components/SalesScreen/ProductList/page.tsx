5ebfb633c1c877570e63b6a8445d9a9dcbcef0ff
X1K00 <fcju14@gmail.com>
2025-02-19 12:34:03 -0300

    search engine improvements

--- components/SalesScreen/ProductList/page.tsx
+++ components/SalesScreen/ProductList/page.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Fuse from 'fuse.js';
 
 import { IoMdSearch } from "react-icons/io";
 import ItemList from './ItemList/page';
@@ -20,21 +21,44 @@ interface Item {
 
 export default function ProductList({handleSelectProduct}:any) {
   const [products, setProducts] = useState<Item[]>([])
+  const [query, setQuery] = useState<string>("")
+
+  // Configurando o Fuse.js
+  const fuse = new Fuse(products, {
+    keys: ['name', 'id'],       // Quais campos buscar
+    threshold: 1.0,       // Sensibilidade da busca (0 = precisa ser igual, 1 = tudo é parecido)
+    distance: 100         // Máxima distância permitida entre os caracteres
+  })
+
+  // Função de pesquisa
+  const handleSearch = (input: string) => {
+    setQuery(input)
+    if (input.trim() === '') {
+      setProducts(products)
+    } else{
+      const result = fuse.search(input).map((res) => res.item)
+      setProducts(result)
+    }
+  }
+
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try{
-        //const response = await fetch('/data/database.json').then() // Faz a requisição
-        //const data = await response.json();  // Converte a resposta para JSON
-        const data = await getAllProducts()
-
-        setProducts(data)
-      } catch (error){
-        console.log("Erro na requisição:", error) // Trata erros
-      }
-    };
-    fetchProducts()
-  }, [])
+    if (query.trim() === ""){
+      const fetchProducts = async () => {
+        try{
+          //const response = await fetch('/data/database.json').then() // Faz a requisição
+          //const data = await response.json();  // Converte a resposta para JSON
+          const data = await getAllProducts()
+          console.log()
+  
+          setProducts(data)
+        } catch (error){
+          console.log("Erro na requisição:", error) // Trata erros
+        }
+      };
+      fetchProducts()
+    }
+  }, [query])
 
   return (
       <div className=" flex flex-col overflow-hidden min-w-[500px]">
@@ -43,7 +67,9 @@ export default function ProductList({handleSelectProduct}:any) {
           <div className="flex">
             <input
               type="text"
+              onChange={(e) => handleSearch(e.target.value)}
               placeholder="Digite o produto..."
+              value={query}
               className="flex-grow bg-[#198A83] placeholder-[#46b0a9] text-white text-lg h-10 pl-2 hover:bg-[#189890]"
             />
             <button className="w-8 bg-[#198A83] text-white text-xl flex items-center justify-center hover:bg-[#19a097]">
